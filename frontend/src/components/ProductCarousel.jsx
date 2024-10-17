610b7e06b54365cf60d43399e8937599dde107a5
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2024-10-17 01:06:55 -0700

    bug fix - removed loader from Carousel

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -1,24 +1,21 @@
-import { Link } from 'react-router-dom'
-import { Carousel, Image } from 'react-bootstrap'
-import Loader from './Loader'
-import Message from './Message'
-import { useGetTopProductsQuery } from '../slices/productsApiSlice'
+import { Link } from 'react-router-dom';
+import { Carousel, Image } from 'react-bootstrap';
+import Message from './Message';
+import { useGetTopProductsQuery } from '../slices/productsApiSlice';
 
 const ProductCarousel = () => {
-  const { data: products, error, isLoading } = useGetTopProductsQuery()
+  const { data: products, isLoading, error } = useGetTopProductsQuery();
 
-  return isLoading ? (
-    <Loader />
-  ) : error ? (
-    <Message variant="danger">{error}</Message>
+  return isLoading ? null : error ? (
+    <Message variant='danger'>{error?.data?.message || error.error}</Message>
   ) : (
-    <Carousel pause="hover" className="bg-primary mb-4">
+    <Carousel pause='hover' className='bg-primary mb-4'>
       {products.map((product) => (
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
             <Image src={product.image} alt={product.name} fluid />
-            <Carousel.Caption className="carousel-caption">
-              <h2>
+            <Carousel.Caption className='carousel-caption'>
+              <h2 className='text-white text-right'>
                 {product.name} (${product.price})
               </h2>
             </Carousel.Caption>
@@ -26,7 +23,7 @@ const ProductCarousel = () => {
         </Carousel.Item>
       ))}
     </Carousel>
-  )
-}
+  );
+};
 
-export default ProductCarousel
+export default ProductCarousel;
